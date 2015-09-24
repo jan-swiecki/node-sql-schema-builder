1dad3626ef29b482503579b0c8b3aeaf75c33dde
Jan Święcki <jan.swiecki@gmail.com>
2015-09-25 00:06:57 +0200

    updates

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 Autowire = require("autowire");
 Autowire.addImportPath("./lib/types");
+Autowire.alias("_", "lodash");
 
-Autowire(function(_, Table, Column, Type) {
+Autowire(function(_, Table, Column, Type, ForeignKey, InitTypes) {
 
-  var table = Table("user").addColumns(
+  var company = Table("company").addColumns(
+    Column("id", Type.UUID.primaryKey()),
+    Column("name", Type.VARCHAR(64).notNull())
+  );
+
+  var user = Table("user").addColumns(
     Column("id", Type.UUID.primaryKey()),
     Column("email", Type.VARCHAR(64).notNull()),
     Column("password", Type.VARCHAR(255).notNull())
+  ).addForeignKeys(
+    ForeignKey("company", company, "id")
   );
 
-  var sql = table.toSql();
+  var sql = user.toSql();
 
   console.log(sql);
 
