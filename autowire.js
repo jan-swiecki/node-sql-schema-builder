f27896e4db309adef963e3ab46d5895bfb655217
Jan Święcki <jan.swiecki@gmail.com>
2015-10-07 19:24:23 +0200

    Autowire v2.2 compatibility

--- autowire.js
+++ autowire.js
@@ -1,9 +1,4 @@
 var Autowire = require("autowire");
-Autowire.addImportPath("./lib");
-Autowire.addImportPath("./lib/types");
-Autowire.addImportPath("./lib/validator");
-Autowire.addImportPath("./lib/orm_generator");
-Autowire.alias("_", "lodash");
 Autowire.alias("Promise", "bluebird");
 
 module.exports = Autowire;
